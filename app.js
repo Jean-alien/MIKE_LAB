45e8bc4d9fe6e90d9f2978029638cb260b65caf1
Myeongji An <marvel0855@gmail.com>
2024-02-08 16:48:20 -0600

    test commit

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 
-app.get('/html', function (req, res) {
-  res.sendFile(__dirname + '/enneagram-test-result.html');
+app.get('/', (req, res) => {
+  res.send(`
+  <h1>Type 9: "Peacemaker"</h1>
+  <p>I am Type Nine, which is known as a mediator of a group!</p>
+`);
 })
 
 app.listen(port, () => {
